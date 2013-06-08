0bda755e34ac8fa751fa138dd3c427c02e82b43c
Gudek <gudek8@gmail.com>
2013-06-08 13:18:00 +0200

    ciekawe czy dziala

---
+++ app/models/url.rb
@@ -0,0 +1,15 @@
+#class CreateUrls < ActiveRecord::Migration
+#  def self.up
+#    create_table :urls do |t|
+#      t.string :url
+#      t.timestamps
+#    end
+#  end
+#  def self.down
+#    drop_table :urls
+# end
+#end
+class Url < ActiveRecord::Base
+  validates :url, :presence => true
+  attr_accessible :url
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shorty::Application.config.secret_token = 'da44ecdac7b3e68cac4d8ed8315f046b71a3253b4e2222e1a559e5a2f53c919ae53a90e092b1f8751202cbc9d3a27a9a7aec892674fe68caa47354573d27a579'
